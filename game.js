5999d2a5983a337d2dc99d7f55f32b568ee38a42
Daria Vasylieva <daria.vasylieva@ivkhk.ee>
2025-11-20 19:48:41 +0200

    Взлет мяча под случайным углом

--- game.js
+++ game.js
@@ -11,6 +11,8 @@ let game = {
     blocks: [],
     rows: 4,
     cols: 8,
+    width: 640,
+    height: 360,
     sprites: {
         background: null,
         ball: null,
@@ -72,6 +74,7 @@ let game = {
         });
     },
     render() {
+        this.ctx.clearRect(0, 0, this.width, this.height);
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, 0, 0, this.ball.width, this.ball.height,
             this.ball.x, this.ball.y, this.ball.width, this.ball.height
@@ -90,10 +93,14 @@ let game = {
             this.create();
             this.run();
         });
+    },
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 };
 
 game.ball = {
+    dx: 0,
     dy: 0,
     velocity: 3,
     x: 320,
@@ -102,12 +109,16 @@ game.ball = {
     height: 20,
     start() {
         this.dy = -this.velocity;
+        this.dx = game.random(-this.velocity, this.velocity); 
     },
     move() {
         if (this.dy) {
             this.y += this.dy;
         }
+        if (this.dx) {
+            this.x += this.dx;
     }
+}
 };
 
 game.platform = {
